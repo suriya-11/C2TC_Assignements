8c8b72fcb5fd4fa4b0a3211f1372cc324e9ca6b4
suriya-11 <suryasuriya716@gmail.com>
2025-07-22 21:58:13 +0530

    Create Student.java

---
+++ Assignment1/Student.java
@@ -0,0 +1,22 @@
+package Entityclassassignment1;
+
+public class Student {
+	String name;
+    int age;
+    int rollNumber;
+
+    // Constructor to initialize student details
+    public Student(String name, int age, int rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+
+    // Method to display student details
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("----------------------");
+    }
+}
