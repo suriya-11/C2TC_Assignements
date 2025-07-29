85b75592ec780b712cf960d0d84d51b13d4dbadf
suriya-11 <suryasuriya716@gmail.com>
2025-07-29 09:28:44 +0530

    Update Account.java

--- Assignment4/Account.java
+++ Assignment4/Account.java
@@ -1,1 +1,25 @@
+package assignemnt4;
 
+public  abstract class Account {
+	
+	 protected String accountHolder;
+	 protected double balance;
+
+	 public Account(String accountHolder, double initialBalance) {
+	     this.accountHolder = accountHolder;
+	     this.balance = initialBalance;
+	     Bank.incrementAccounts(); // Increase total accounts count
+	 }
+
+	 // Abstract Methods
+	 public abstract void deposit(double amount);
+	 public abstract void withdraw(double amount);
+	 public abstract double getBalance();
+
+	 // Common Method
+	 public void displayAccountInfo() {
+	     System.out.println("Account Holder: " + accountHolder);
+	     System.out.println("Balance: " + balance);
+	 }
+
+}
