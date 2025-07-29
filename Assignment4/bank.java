cd09f3536d69bd6309cfb8a8d775b9b9e09befb3
suriya-11 <suryasuriya716@gmail.com>
2025-07-29 09:30:43 +0530

    Update bank.java

--- Assignment4/bank.java
+++ Assignment4/bank.java
@@ -1,1 +1,18 @@
+package assignemnt4;
+
+public class Bank {
+	 // Static variable to track total accounts
+    private static int totalAccounts = 0;
+
+    // Increment total accounts whenever a new account is created
+    public static void incrementAccounts() {
+        totalAccounts++;
+    }
+
+    // Static method to get total accounts
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+
+}
 
